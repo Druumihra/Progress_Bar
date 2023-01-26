a344db50943848042796754290bf02d0d89b1625
ReimarPB <reimarpb@gmail.com>
2023-01-26 14:49:20 +0100

    Add loading percentage

--- Loading pub/Loading pub/Program.cs
+++ Loading pub/Loading pub/Program.cs
@@ -1,5 +1,12 @@
-﻿Console.WriteLine("Pick what prime number you want");
+﻿Console.Clear();
+Console.WriteLine("Pick what prime number you want");
 int pickedNumber = Convert.ToInt32(Console.ReadLine());
+
+PrimeFinder.ProgressEvent += (count) => {
+    int percentage = (int)((double)count/ pickedNumber * 100.0);
+    Console.SetCursorPosition(0, 3);
+    Console.Write(percentage.ToString() + "%");
+};
 
 int result = PrimeFinder.FindPrime(pickedNumber);
 
