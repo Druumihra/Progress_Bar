0d5c7b1a34dc4379c06e9e6527be0b1b59578305
MaggH-Jones <102585564+MaggH-Jones@users.noreply.github.com>
2023-01-26 13:59:30 +0100

    start of Loading pub primenumber finder

---
+++ Loading pub/Loading pub/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+int currentNumber = 1;
+
+Console.WriteLine("Pick what prime number you want");
+int pickedNumber = Convert.ToInt32(Console.ReadLine());
+
+while (count < pickedNumber){
+    if (isPrime(currentNumber)) {
+        count++;
+    }
+    currentNumber++;
+}
+currentNumber--;
+
+Console.WriteLine($"Result: {currentNumber}");
+
+bool isPrime(int number)
+{
+    if (number == 1) return false;
+    if (number == 2) return true;
+
+    var limit = Math.Ceiling(Math.Sqrt(number));
+
+    for (int i = 2; i <= limit; ++i)
+        if (number % i == 0)
+            return false;
+    return true;
+
+}
