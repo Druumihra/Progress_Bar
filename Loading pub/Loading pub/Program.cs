25a069dc02e1f5cec3583fa56ba5c1c52086c57c
ReimarPB <reimarpb@gmail.com>
2023-01-26 14:32:56 +0100

    Extract to class and create event

--- Loading pub/Loading pub/Program.cs
+++ Loading pub/Loading pub/Program.cs
@@ -1,29 +1,7 @@
-﻿int count = 0;
-int currentNumber = 1;
-
-Console.WriteLine("Pick what prime number you want");
+﻿Console.WriteLine("Pick what prime number you want");
 int pickedNumber = Convert.ToInt32(Console.ReadLine());
 
-while (count < pickedNumber){
-    if (isPrime(currentNumber)) {
-        count++;
-    }
-    currentNumber++;
-}
-currentNumber--;
+int result = PrimeFinder.FindPrime(pickedNumber);
 
-Console.WriteLine($"Result: {currentNumber}");
+Console.WriteLine($"Result: {result}");
 
-bool isPrime(int number)
-{
-    if (number == 1) return false;
-    if (number == 2) return true;
-
-    var limit = Math.Ceiling(Math.Sqrt(number));
-
-    for (int i = 2; i <= limit; ++i)
-        if (number % i == 0)
-            return false;
-    return true;
-
-}
