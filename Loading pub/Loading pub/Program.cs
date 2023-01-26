caabadb3f7c4f938c21179c0aecf73352de94c8f
Druumihra <teis.bindner@laerke.net>
2023-01-27 00:00:53 +0100

    added progress bar

--- Loading pub/Loading pub/Program.cs
+++ Loading pub/Loading pub/Program.cs
@@ -2,13 +2,62 @@
 Console.WriteLine("Pick what prime number you want");
 int pickedNumber = Convert.ToInt32(Console.ReadLine());
 
-PrimeFinder.ProgressEvent += (count) => {
-    int percentage = (int)((double)count/ pickedNumber * 100.0);
-    Console.SetCursorPosition(0, 3);
-    Console.Write(percentage.ToString() + "%");
+
+
+PrimeFinder.ProgressEvent += (count) =>
+{
+    int percentage = (int)((double)count / pickedNumber * 100.0);
+    switch (percentage)
+    {
+        case < 10:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[----------]");
+            break;
+        case < 20:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[#---------]");
+            break;
+        case < 30:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[##--------]");
+            break;
+        case < 40:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[###-------]");
+            break;
+        case < 50:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[####------]");
+            break;
+        case < 60:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[#####-----]");
+            break;
+        case < 70:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[######----]");
+            break;
+        case < 80:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[#######---]");
+            break;
+        case < 90:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[########--]");
+            break;
+        case < 100:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[#########-]");
+            break;
+        case 100:
+            Console.SetCursorPosition(0, 3);
+            Console.Write("[##########]");
+            break;
+
+    }
 };
 
 int result = PrimeFinder.FindPrime(pickedNumber);
-
+Console.SetCursorPosition(0, 4);
 Console.WriteLine($"Result: {result}");
 
