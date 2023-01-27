08b56672bb87d2c0d148ce97db8015973e6b285e
Druumihra <teis.bindner@laerke.net>
2023-01-27 08:08:25 +0100

    rewrite

--- Loading pub/Loading pub/Program.cs
+++ Loading pub/Loading pub/Program.cs
@@ -6,55 +6,10 @@ int pickedNumber = Convert.ToInt32(Console.ReadLine());
 
 PrimeFinder.ProgressEvent += (count) =>
 {
+    Console.CursorVisible = false;
     int percentage = (int)((double)count / pickedNumber * 100.0);
-    switch (percentage)
-    {
-        case < 10:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[----------]");
-            break;
-        case < 20:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[#---------]");
-            break;
-        case < 30:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[##--------]");
-            break;
-        case < 40:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[###-------]");
-            break;
-        case < 50:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[####------]");
-            break;
-        case < 60:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[#####-----]");
-            break;
-        case < 70:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[######----]");
-            break;
-        case < 80:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[#######---]");
-            break;
-        case < 90:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[########--]");
-            break;
-        case < 100:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[#########-]");
-            break;
-        case 100:
-            Console.SetCursorPosition(0, 3);
-            Console.Write("[##########]");
-            break;
-
-    }
+    Console.SetCursorPosition(0, 3);
+    Console.Write("[" + new string('#', percentage / 10) + new string('-', 10 - percentage / 10) + "]");
 };
 
 int result = PrimeFinder.FindPrime(pickedNumber);
